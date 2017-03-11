fdbef626704d1da387654a22512895f1a32e418b
Vaclav Stebra <vstebra@gmail.com>
2017-03-11 16:50:53 +0100

    Firebase setup

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
@@ -1,7 +1,17 @@
 package cz.muni.fi.a2p06.stolencardatabase;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        this.testWrite();
+        this.testRead(textView);
+    }
+
+    private void testWrite() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+    }
+
+    private void testRead(final TextView view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                view.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
