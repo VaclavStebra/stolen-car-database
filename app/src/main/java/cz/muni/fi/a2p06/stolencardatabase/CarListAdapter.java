6c92057697e88d7cfd4515f36163df0137cc28eb
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-12 23:07:50 +0100

    CarListAdapter init

---
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/CarListAdapter.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.a2p06.stolencardatabase;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.Query;
+
+import butterknife.BindView;
+import cz.muni.fi.a2p06.stolencardatabase.entity.Car;
+
+/**
+ * Created by robert on 12.3.2017.
+ */
+
+public class CarListAdapter extends FirebaseRecyclerAdapter<Car, CarListAdapter.CarItemHolder> {
+
+    public CarListAdapter(Class<Car> modelClass, int modelLayout, Class<CarItemHolder> viewHolderClass, Query ref) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+    }
+
+    @Override
+    protected void populateViewHolder(CarItemHolder viewHolder, Car model, int position) {
+
+    }
+
+    static class CarItemHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.car_image)
+        ImageView mCarImage;
+
+        public CarItemHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
