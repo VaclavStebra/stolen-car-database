12375ad6cc95eb0b7be54f6942ec953f9f17431f
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-14 22:42:46 +0100

    CarListListener

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
@@ -2,13 +2,9 @@ package cz.muni.fi.a2p06.stolencardatabase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 
@@ -21,35 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        //this.testWrite();
-        this.testRead(textView);
-        //this.addCar();
+//        TextView textView = (TextView) findViewById(R.id.text_view);
+//        //this.testWrite();
+//        this.testRead(textView);
+//        //this.addCar();
     }
 
-    private void testWrite() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
-
-    }
-
-    private void testRead(final TextView view) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                view.setText(value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-    }
-
+    //    private void testWrite() {
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, World!");
+//
+//    }
+//
+//    private void testRead(final TextView view) {
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                String value = dataSnapshot.getValue(String.class);
+//                view.setText(value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });
+//    }
+//
     // Just an example of how to write to firebase "list"
     private void addCar() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -59,18 +55,18 @@ public class MainActivity extends AppCompatActivity {
 
         // prepare car object
         Car car = new Car();
-        car.setColor("modra tmava");
+        car.setColor("cervena");
         car.setEngine("2.0 TDI");
-        car.setManufacturer("Volkswagen");
-        car.setPhotoUrl("gs://stolen-car-database.appspot.com/PAS0.jpg");
+        car.setManufacturer("Skoda");
+        car.setPhotoUrl("gs://stolen-car-database.appspot.com/Skoda_Octavia.jpg");
         car.setProductionYear(2002);
         car.setRegno("1XY1234");
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2015);
-        cal.set(Calendar.MONTH, 3);
-        cal.set(Calendar.DAY_OF_MONTH, 13);
+        cal.set(Calendar.YEAR, 2016);
+        cal.set(Calendar.MONTH, 5);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
         car.setStolenDate(cal.getTimeInMillis());
-        car.setModel("PASSAT VARIANT");
+        car.setModel("Octavia");
         car.setVin("WVWZZZ12345624");
         Coordinates coords = new Coordinates();
         coords.setLat(49.210019);
