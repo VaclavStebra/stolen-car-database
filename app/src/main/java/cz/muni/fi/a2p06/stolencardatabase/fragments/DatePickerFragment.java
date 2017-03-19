d0c500a813530e780208d5808e6bd60003c10561
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-19 21:22:03 +0100

    DatePickerFragment completed

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/DatePickerFragment.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/DatePickerFragment.java
@@ -3,7 +3,6 @@ package cz.muni.fi.a2p06.stolencardatabase.fragments;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.os.Bundle;
 
 import java.util.Calendar;
@@ -14,26 +13,28 @@ import java.util.Calendar;
 
 public class DatePickerFragment extends DialogFragment {
 
-    DatePickerDialog.OnDateSetListener mListener;
+    private DatePickerDialog.OnDateSetListener mListener;
+    private Calendar mCalendar;
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            mListener = (DatePickerDialog.OnDateSetListener) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement OnDateSetListener");
-        }
+    public void setCalendar(Calendar calendar) {
+        this.mCalendar = calendar;
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.mListener = listener;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        if (mCalendar == null) {
+            mCalendar = Calendar.getInstance();
+        }
+
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int day = mCalendar.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), mListener, year, month, day);
