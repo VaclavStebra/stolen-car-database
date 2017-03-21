1ef8fe0096c4d7f89e042b43dd6a44213108f32e
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-21 19:45:43 +0100

    CarPhotoStepFragment - intent, layout

---
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarPhotoStepFragment.java
@@ -0,0 +1,95 @@
+package cz.muni.fi.a2p06.stolencardatabase.fragments;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.VerificationError;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cz.muni.fi.a2p06.stolencardatabase.R;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CarPhotoStepFragment extends Fragment implements Step {
+
+    private final int PICK_IMAGE_REQUEST = 1;
+
+    @BindView(R.id.add_photo_btn)
+    Button mAddPhotoBtn;
+    @BindView(R.id.car_photo_view)
+    ImageView mCarPhoto;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_car_photo_step, container, false);
+        ButterKnife.bind(this, view);
+
+        mAddPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                photoIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(photoIntent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            if (selectedImage != null) {
+                try {
+                    // TODO: version for API 19-
+                    Bitmap image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
+                    mCarPhoto.setImageBitmap(image);
+                } catch (IOException e) {
+                    Log.e(TAG, "onActivityResult: Image loading error ", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public VerificationError verifyStep() {
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
+    }
+}
