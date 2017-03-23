e3bb271360820f8d70df808e2067b29a2eda40c6
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-23 20:55:43 +0100

    CarLocationStepFragment - layout, MapView implementation

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarLocationStepFragment.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarLocationStepFragment.java
@@ -2,6 +2,7 @@ package cz.muni.fi.a2p06.stolencardatabase.fragments;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -20,6 +21,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
@@ -37,15 +39,18 @@ import static com.google.android.gms.location.places.ui.PlacePicker.getPlace;
  * A simple {@link Fragment} subclass.
  */
 public class CarLocationStepFragment extends Fragment implements Step, OnMapReadyCallback {
+    // TODO: Check if any permissions are needed
 
     @BindView(R.id.location_btn)
     Button mSetLocationBtn;
-    @BindView(R.id.textView)
-    TextView mLocation;
+    @BindView(R.id.place_text)
+    TextView mPlaceTxt;
     @BindView(R.id.mapView)
     MapView mMapView;
 
     private GoogleMap mMap;
+    private Marker mMarker;
+
     private final int PLACE_PICKER_REQUEST = 1;
 
     @Override
@@ -68,9 +73,11 @@ public class CarLocationStepFragment extends Fragment implements Step, OnMapRead
             }
         });
 
-//        mMapView.setVisibility(View.GONE);
+        mMapView.setVisibility(View.GONE);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
+        mMapView.setClickable(false);
+
         return view;
     }
 
@@ -79,14 +86,63 @@ public class CarLocationStepFragment extends Fragment implements Step, OnMapRead
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK && data != null) {
-            Place place = getPlace(getContext(), data);
-            mLocation.setText(String.format("Place: %s", place.getName()));
+            final Place place = getPlace(getContext(), data);
+
+            mPlaceTxt.setText(String.format("Location: %s", place.getName()));
+            mPlaceTxt.setTypeface(Typeface.DEFAULT);
+
             LatLng carPosition = place.getLatLng();
-            mMap.addMarker(new MarkerOptions().position(carPosition).title("Car position"));
+            if (mMarker == null) {
+                mMarker = mMap.addMarker(new MarkerOptions().position(carPosition).title("Car position"));
+            } else {
+                mMarker.setPosition(carPosition);
+            }
             mMap.moveCamera(CameraUpdateFactory.newLatLng(carPosition));
-            mMapView.onResume();
+
             mMapView.setVisibility(View.VISIBLE);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
     }
 
     @Override
