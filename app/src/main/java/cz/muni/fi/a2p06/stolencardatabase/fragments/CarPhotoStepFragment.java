bfaa2b1f8136381dbf0009da0c81d9af84b9e782
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-26 18:54:21 +0200

    CarPhotoStepFragment.java - storing data

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarPhotoStepFragment.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarPhotoStepFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
@@ -30,6 +31,7 @@ public class CarPhotoStepFragment extends Fragment implements BlockingStep {
     // TODO: Check if any permissions are needed
 
     private static final int PICK_IMAGE_REQUEST = 1;
+    private static final String CAR_PHOTO = "car_photo";
 
     private Car mCar;
 
@@ -37,6 +39,8 @@ public class CarPhotoStepFragment extends Fragment implements BlockingStep {
     Button mAddPhotoBtn;
     @BindView(R.id.car_photo_view)
     ImageView mCarPhoto;
+    @BindView(R.id.photo_text)
+    TextView mPhotoText;
 
     private Uri mPhotoUri;
 
@@ -54,6 +58,13 @@ public class CarPhotoStepFragment extends Fragment implements BlockingStep {
         View view = inflater.inflate(R.layout.fragment_car_photo_step, container, false);
         ButterKnife.bind(this, view);
 
+        if (savedInstanceState != null) {
+            mPhotoUri = savedInstanceState.getParcelable(CAR_PHOTO);
+            showPhoto();
+        } else {
+            mCarPhoto.setVisibility(View.GONE);
+        }
+
         mAddPhotoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +77,36 @@ public class CarPhotoStepFragment extends Fragment implements BlockingStep {
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            mPhotoUri = data.getData();
+            showPhoto();
+        }
+    }
+
+    private void showPhoto() {
+        if (mPhotoUri != null) {
+            mPhotoText.setVisibility(View.GONE);
+            mCarPhoto.setVisibility(View.VISIBLE);
+            mCarPhoto.setImageURI(mPhotoUri);
+//            Glide.with(getActivity())
+//                        .load(mPhotoUri)
+//                        .asBitmap()
+//                        .placeholder(R.drawable.car_placeholder)
+//                        .into(mCarPhoto);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mPhotoUri != null)
+            outState.putParcelable(CAR_PHOTO, mPhotoUri);
+        super.onSaveInstanceState(outState);
+    }
+
     public static CarPhotoStepFragment newInstance(Car car) {
         CarPhotoStepFragment fragment = new CarPhotoStepFragment();
         Bundle args = new Bundle();
@@ -75,31 +116,18 @@ public class CarPhotoStepFragment extends Fragment implements BlockingStep {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
-            mPhotoUri = data.getData();
-
-            if (mPhotoUri != null) {
-                mCarPhoto.setImageURI(mPhotoUri);
-            }
-        }
-    }
-
-    @Override
     public VerificationError verifyStep() {
         return null;
     }
 
     @Override
     public void onSelected() {
-
+        //
     }
 
     @Override
     public void onError(@NonNull VerificationError error) {
-
+        //
     }
 
     @Override
