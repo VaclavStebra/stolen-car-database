b78740e9b60042c987dd79eb659b9f7544fedc03
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-04-04 11:47:31 +0200

    search menu in CarListFragment toolbar

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarListFragment.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/fragments/CarListFragment.java
@@ -2,11 +2,14 @@ package cz.muni.fi.a2p06.stolencardatabase.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -26,6 +29,12 @@ public class CarListFragment extends Fragment implements CarListAdapter.CarItemH
 
     public CarListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -54,6 +63,11 @@ public class CarListFragment extends Fragment implements CarListAdapter.CarItemH
         });
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
     }
 
     @Override
