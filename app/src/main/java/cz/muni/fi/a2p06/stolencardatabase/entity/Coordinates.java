dfecf2e28c17feb19e1f9542844b8a1e25f09a47
rduriancik <r.duriancik@gmail.com>
2017-05-01 11:08:29 +0200

    equals and hashCode methods implemented in Coordinates

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/entity/Coordinates.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/entity/Coordinates.java
@@ -69,4 +69,27 @@ public class Coordinates implements Parcelable {
             return new Coordinates[size];
         }
     };
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinates)) return false;
+
+        Coordinates that = (Coordinates) o;
+
+        if (Double.compare(that.lat, lat) != 0) return false;
+        return Double.compare(that.lon, lon) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(lat);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(lon);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
