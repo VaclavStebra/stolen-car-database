1aae860360b1f47ca41a6e85d65e7682fe8004d5
Vaclav Stebra <vstebra@gmail.com>
2017-05-30 13:42:05 +0200

    Display reported location in map
    * custom mapview to allow scrolling / zoom inside scroll view
    * removed possibility to open google maps since it does not show all markers

---
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/views/CustomMapView.java
@@ -0,0 +1,56 @@
+package cz.muni.fi.a2p06.stolencardatabase.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewParent;
+
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
+
+public class CustomMapView extends MapView {
+
+    private ViewParent mViewParent;
+
+    public CustomMapView(Context context) {
+        super(context);
+    }
+
+    public CustomMapView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public CustomMapView(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+    }
+
+    public CustomMapView(Context context, GoogleMapOptions googleMapOptions) {
+        super(context, googleMapOptions);
+    }
+
+    public void setViewParent(final ViewParent viewParent) {
+        mViewParent = viewParent;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (null == mViewParent) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                } else {
+                    mViewParent.requestDisallowInterceptTouchEvent(true);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (null == mViewParent) {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                } else {
+                    mViewParent.requestDisallowInterceptTouchEvent(false);
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(event);
+    }
+}
