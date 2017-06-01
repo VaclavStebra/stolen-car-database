c04e47cec53895af37166f0df2aa12c84d39f128
rduriancik <r.duriancik@gmail.com>
2017-06-01 16:05:06 +0200

    Imports

--- app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
+++ app/src/main/java/cz/muni/fi/a2p06/stolencardatabase/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
